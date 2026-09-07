ca123b41aaa30bacdd934a44151d1baa82600851
Aissatou255 <diaaicha2021@gmail.com>
2026-09-07 11:46:41 +0000

    Corrige migration products qui creait hotels par erreur

--- database/migrations/2026_09_03_112701_create_products_table.php
+++ database/migrations/2026_09_03_112701_create_products_table.php
@@ -6,26 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-   public function up(): void
-{
-    Schema::create('hotels', function (Blueprint $table) {
-        $table->id();
-        $table->string('name');
-        $table->string('address');
-        $table->string('email')->nullable();
-        $table->string('phone')->nullable();
-        $table->decimal('price_per_night', 10, 2);
-        $table->string('currency')->default('XOF');
-        $table->string('photo_url')->nullable();
-        $table->timestamps();
-    });
-}
-    /**
-     * Reverse the migrations.
-     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->decimal('price', 8, 2);
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
     public function down(): void
     {
         Schema::dropIfExists('products');
