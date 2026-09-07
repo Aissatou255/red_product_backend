23c2828de6edfa3c56da5b1b7eab5d6c820f48b3
Aissatou255 <diaaicha2021@gmail.com>
2026-09-07 10:19:00 +0000

    Ajout route temporaire de nettoyage DB

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/reset-database-temp-xyz123', function () {
+    Illuminate\Support\Facades\Schema::dropAllTables();
+    return "Base de données nettoyée !";
+});
